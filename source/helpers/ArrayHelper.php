7a96d6671d3c1e81bd14430336f8fcd0b587b9a3
Игнатьев Евгений <ignatiev@po-korf.ru>
2015-07-19 12:51:05 +0300

    В хелпер ArrayHelper добавлен метод ArrayHelper::isArray(), проверяющий переменную на массив.

--- source/helpers/ArrayHelper.php
+++ source/helpers/ArrayHelper.php
@@ -10,6 +10,17 @@ namespace expsys\yii2\helpers;
 class ArrayHelper extends yii\helpers\ArrayHelper
 {
 	/**
+	 * @brief Возвращает true, если переменная $var является массивом, false - в ином случае.
+	 * @param mixed $var Проверяемая переменная.
+	 * @return bool
+	 */
+
+	public static function isArray($var)
+	{
+		return is_array($var);
+	}
+
+	/**
 	 * @brief Добавляет элемент $element в массив $array.
 	 * @param array $array Целевой массив
 	 * @param mixed $element Добавляемый элемент
